8adca61cfd6bae0a8a405cdb307b700a048944bb
Jacob <Jacalz@users.noreply.github.com>
2020-11-24 16:53:14 +0100

    Much more efficient and clean Fibonacci algorithm

--- math/fibonacci/regular/main.go
+++ math/fibonacci/regular/main.go
@@ -3,26 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var num1, num2, nextTerm int64
-	var n int
-	num1 = 0
-	num2 = 1
-	fmt.Print("Enter number of passes: ")
-	fmt.Scanln(&n)
-	fmt.Println("Fibonacci series:")
+	var n1, n2 uint = 0, 1
+	var next, iter uint
 
-	for i := 1; i <= n; i++ {
-		if i == 1 {
-			fmt.Println(num1)
-			continue
-		}
-		if i == 2 {
-			fmt.Println(num2)
-			continue
-		}
-		nextTerm = num1 + num2
-		num1 = num2
-		num2 = nextTerm
-		fmt.Println(nextTerm)
+	fmt.Print("Enter amount of numbers to print: ")
+	fmt.Scanln(&iter)
+
+	for i := uint(0); i < iter; i++ {
+		fmt.Println(next)
+		next = n1 + n2
+		n2 = n1
+		n1 = next
 	}
 }
+
